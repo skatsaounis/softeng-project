829824189aaeb63480b447ce55a7891c350cec92
stammy31 <katsaouniss@gmail.com>
2016-03-10 09:53:01 +0200

    Add new template for servers. Implement more methods.

--- code/Proxy/BlockingServer.java
+++ code/Proxy/BlockingServer.java
@@ -1,26 +1,51 @@
 public class BlockingServer {
+	Database database;
 	
-	
-	public BlockingServer() {
-		// empty
+	public BlockingServer(Database database) {
+		this.database = database;
 	}
 	
 	public boolean is_blocking(int src, int dst){
-		System.out.println("1. is_blocking");
-		return true;
+		System.out.println("1. is_blocking (\\/)");
+		return database.search_is_blocking(src, dst);
 	}
 	
 	public boolean blocking_check(int caller, int[] fwd_chain){
-		System.out.println("2. blocking_check");
+		System.out.println("2. blocking_check (\\/)");
+		int i;
+		int dst;
+		for(i=0; i<(fwd_chain.length-1); i++){
+			dst = fwd_chain[i];
+			if(database.search_is_blocking(caller, dst)){
+				System.out.println("Blocked by someone in fwd_chain");
+				return false;
+			}
+		}
+		/* To be added -- Check if anyone in chain is blocked by its previous */
+		System.out.println("Acceptable Call");
 		return true;
 	}
 	
-	public void request_block(int src, int dst){
-		System.out.println("3. request_block");
+	public boolean request_block(int src, int dst){
+		System.out.println("3. request_block (\\/)");
+		if(database.search_user(dst)){
+			if(!database.search_is_blocking(src, dst))
+				database.set_block(src, dst);
+			return true;
+		}
+		else
+			return false;
 	}
 	
-	public void remove_block(int src, int dst){
-		System.out.println("4. remove_block");
+	public boolean remove_block(int src, int dst){
+		System.out.println("4. remove_block (\\/)");
+		if(database.search_user(dst)){
+			if(database.search_is_blocking(src, dst))
+				database.remove_block(src, dst);
+			return true;
+		}
+		else
+			return false;
 	}
 	
 }
---
+++ code/Proxy/Proxy.java
@@ -0,0 +1,63 @@
+
+public class Proxy {
+
+	public Proxy() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		int[] fwd_chain = {1};
+		int src = 1;
+		int dst = 1;
+		int username = 1;
+		int name = 1;
+		int my_plan = 1;
+		int timestamp = 1;
+		
+		/*String db = "test.db";
+		Connection c = null;*/
+		Database database = new Database();
+		BlockingServer blocking_server = new BlockingServer(database);
+		ForwardingServer forwarding_server = new ForwardingServer(blocking_server, database);
+		BillingServer billing_server = new BillingServer(database);
+		ProxyServer proxy_server = new ProxyServer(blocking_server, forwarding_server, billing_server, database);
+		
+		
+		System.out.println("Proxy Server");
+		proxy_server.call_start(src, dst);
+		proxy_server.request_block(src, dst);
+		proxy_server.remove_block(src, dst);
+		proxy_server.forwarding_registration(src, dst);
+		proxy_server.forwarding_removal(username);
+		proxy_server.select_plan(name, my_plan);
+		proxy_server.total_charge_request(name);
+		proxy_server.call_end(src, dst);
+		System.out.println("---");
+		
+		System.out.println("Blocking Server");
+		blocking_server.is_blocking(src, dst);
+		blocking_server.blocking_check(1, fwd_chain);
+		blocking_server.request_block(src, dst);
+		blocking_server.remove_block(src, dst);
+		System.out.println("---");
+		
+		System.out.println("Forwarding Server");
+		forwarding_server.route(src, dst);
+		forwarding_server.forwarding_registration(src, dst);
+		forwarding_server.forwarding_removal(username);
+		System.out.println("---");
+		
+		/*System.out.println("---");
+		c = database.init_connection(db);
+		database.close_connection(c);*/
+		
+		System.out.println("Billing Server");
+		billing_server.select_plan(username, my_plan);
+		billing_server.call_charge_start(src, dst, timestamp);
+		billing_server.call_charge_end(src, dst, timestamp);
+		billing_server.total_charge(name);
+		System.out.println("---");
+		
+	}
+
+}
