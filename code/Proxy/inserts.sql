f6fb9e866a398c524ae385e9054f74e0ab610a88
stammy31 <katsaouniss@gmail.com>
2016-03-13 12:34:21 +0200

    Create sqlite database schema. Implement set_block as an example of usage.

---
+++ code/Proxy/inserts.sql
@@ -0,0 +1,9 @@
+INSERT INTO user (name,program)
+VALUES ('stamatis',1);
+INSERT INTO user (forwardee,name,program)
+VALUES (1,'ilias',2);
+INSERT INTO user (forwardee,name,program)
+VALUES (1,'thanasis',3);
+
+INSERT INTO call (caller,callee,cost)
+VALUES (1,2,34.5);
