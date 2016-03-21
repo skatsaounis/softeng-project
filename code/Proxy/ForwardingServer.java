bbb7e1d39d982a345ad4759be146da788e41da4e
Thanasis Papoutsidakis <PwnzorBot4000@gmail.com>
2016-03-21 22:11:10 +0200

    Prevent forwarding loops during forwarding registration.
    Some minor gantt diagram edits also commited.

--- code/Proxy/ForwardingServer.java
+++ code/Proxy/ForwardingServer.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class ForwardingServer {
 	BlockingServer blocking;
 	Database database;
-	
+
 	public ForwardingServer(BlockingServer blocking, Database database) {
 		this.blocking = blocking;
 		this.database = database;
@@ -12,7 +12,7 @@ public class ForwardingServer {
 	private boolean equals(int src, int dst){
 		return (src == dst);
 	}
-	
+
 	public int route(int src, int dst){
 		System.out.println("1. route (\\/)");
 		ArrayList<Integer> fwd_chain = database.forwarding_chain(dst);
@@ -25,36 +25,37 @@ public class ForwardingServer {
 		} else
 			return dst;
 	}
-	
+
 	public boolean forwarding_registration(int src, String dst){
 		System.out.println("2. forwarding_registration (\\/)");
-		ArrayList<Integer> list_of_usernames;
 		int dest = database.search_user(dst);
-		if(dest>0){
-			list_of_usernames = database.forwarding_chain(dest);
-			if(list_of_usernames != null){
-				database.set_forwarding(src, dest);
-				return true;
-			}
-		}
-		return false;
+		if (dest <= 0) return false;  // nonexistent user.
+
+        // Confirm that the new forwarding
+        // will not create a forwarding loop.
+        ArrayList<Integer> dest_fwdees = database.forwarding_chain(dest);
+        if (dest_fwdees == null) return false;  // unexpected internal error.
+        if (dest_fwdees.contains(src)) return false;  // loop exists.
+
+        database.set_forwarding(src, dest);
+        return true;
 	}
-	
+
 	public void forwarding_removal(int username){
 		System.out.println("3. forwarding_removal (\\/)");
 		if(database.search_forwarding(username)){
 			database.remove_forwarding(username);
 		}
 	}
-	
+
 	private boolean blocking_check(int caller, ArrayList<Integer> fwd_chain){
 		System.out.println("4. blocking_check (\\/)");
 		return blocking.blocking_check(caller, fwd_chain);
 	}
-	
+
 	private boolean is_self_call(int src, int dst){
 		System.out.println("5. is_self_call (\\/)");
-		return equals(src, dst);	
+		return equals(src, dst);
 	}
-	
+
 }
